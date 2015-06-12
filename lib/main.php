27449bbe3d49ddb1a6cc7c0d4e31bace36894bb0
brian <ardiles.brian@gmail.com>
2015-06-11 23:21:23 -0300

    .

--- lib/main.php
+++ lib/main.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-date_default_timezone_set('America/Argentina/Buenos_Aires');
+//date_default_timezone_set('America/Argentina/Buenos_Aires');
 
 function getData($link)
 {
